9b9f316ed2cda1ae52a34e96d15e0a7979859e0b
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-03-22 20:18:17 +0900

    Added new component

---
+++ src/app/notes/notes-create/notes-create.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notes-create',
+  templateUrl: './notes-create.component.html'
+})
+export class NotesCreateComponent {
+
+}
