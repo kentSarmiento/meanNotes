8f752c0c3debae4b555692406b8f4e5be9d72181
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-03-22 21:40:34 +0900

    Learning: Basic input/output

--- src/app/notes/notes-create/notes-create.component.ts
+++ src/app/notes/notes-create/notes-create.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './notes-create.component.html'
 })
 export class NotesCreateComponent {
+  textareaValue = '';
+  newNote = '';
 
+  onAddNote() {
+    this.newNote = this.textareaValue;
+  }
 }
