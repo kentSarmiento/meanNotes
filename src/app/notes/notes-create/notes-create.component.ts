1497e53a09847ee6f20322eca8773e10c39bddb6
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-03-25 20:02:39 +0900

    Learning: Added components

--- src/app/notes/notes-create/notes-create.component.ts
+++ src/app/notes/notes-create/notes-create.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notes-create',
-  templateUrl: './notes-create.component.html'
+  templateUrl: './notes-create.component.html',
+  styleUrls: ['./notes-create.component.css']
 })
 export class NotesCreateComponent {
   textareaValue = '';
