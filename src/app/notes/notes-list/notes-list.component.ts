f640066a9ecbeb592922e08ded3ae440a69ba5a7
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-04-05 00:07:27 +0900

    Pagination support

--- src/app/notes/notes-list/notes-list.component.ts
+++ src/app/notes/notes-list/notes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component , OnInit, OnDestroy } from "@angular/core";
+import { PageEvent } from '@angular/material/paginator';
 import { Subscription } from 'rxjs';
 
 import { Note } from "../notes.model"
@@ -12,22 +13,38 @@ import { NotesService } from '../notes.service';
 export class NotesListComponent implements OnInit {
   private notesSub : Subscription;
   notes: Note[] = [];
+  page = 1;
+  limit = 5;
+  total = 0;
+  options = [1, 2, 5, 10];
   isLoading = false;
 
   constructor (public notesService: NotesService){}
 
   ngOnInit() {
-    this.notesService.getNotes();
     this.isLoading = true;
-    this.notesSub = this.notesService.getNotesUpdatedListener().
-      subscribe( (notes: Note[]) => {
+    this.notesService.getNotes(this.page, this.limit);
+    this.notesSub = this.notesService
+      .getNotesUpdatedListener()
+      .subscribe( (notebook: { notes: Note[], total: number }) => {
         this.isLoading = false;
-        this.notes = notes;
+        this.total = notebook.total;
+        this.notes = notebook.notes;
       });
   }
 
+  onChangePage(pageInfo: PageEvent) {
+    this.isLoading = true;
+    this.page = pageInfo.pageIndex + 1;
+    this.limit = pageInfo.pageSize;
+    this.notesService.getNotes(this.page, this.limit);
+  }
+
   onDelete(id: string) {
-    this.notesService.deleteNote(id);
+    this.isLoading = true;
+    this.notesService.deleteNote(id).subscribe(() => {
+      this.notesService.getNotes(this.page, this.limit);
+    });
   }
 
   ngOnDestroy() {
