99209a4adb7abeef1c61184e1602ddd29da3a346
Kent Tristan Yves L. Sarmiento <kentsarmiento@gmail.com>
2020-04-18 15:12:18 +0900

    Using ReactiveForms

--- src/app/notes/notes.module.ts
+++ src/app/notes/notes.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from "@angular/core";
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 
@@ -14,7 +14,7 @@ import { AngularMaterialModule } from "../angular-material.module";
   ],
   imports: [
     CommonModule,
-    FormsModule,
+    ReactiveFormsModule,
     AngularMaterialModule,
     RouterModule
   ]
